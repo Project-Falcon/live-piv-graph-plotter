85c5c3795652beff6975e2b4c654268f1be2e708
Kush Makkapati <kush.makkapati@icloud.com>
2023-03-16 17:52:27 +0000

    Make changes to ivplot.py

--- ivplot.py
+++ ivplot.py
@@ -1,33 +1,19 @@
-from itertools import count
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import random
 import math
+import random
+from itertools import count
 
-# Arrays for sensor data
-current = []
-voltage = []
-power = []
-
-x_val = []
-
-index = count() # counter
-
-# Interval (ms)
-intervalValue = 500
-
-fig, axs = plt.subplots(3)
-fig.suptitle('Drone Control')
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.animation import FuncAnimation
 
 
 # Function to be run on every frame
-def updateValues(i):
+def updateValues(_, axs, x_val, index, current, voltage, power):
     # Generate random values
     x_val.append(next(index))
     current.append(random.uniform(1, 45))
     voltage.append(random.uniform(11, 22))
-    power.append(current[-1]*voltage[-1])
+    power.append(current[-1] * voltage[-1])
 
     # Plot the lines
     axs[0].clear()
@@ -36,7 +22,6 @@ def updateValues(i):
     axs[0].plot(x_val, current)
     axs[1].plot(x_val, voltage, color="red")
     axs[2].plot(x_val, power, color="green")
-
 
     # Configure axis labels
     axs[0].set_xlabel("Time (s)")
@@ -47,9 +32,9 @@ def updateValues(i):
     axs[2].set_ylabel("Power")
 
     # Display current data
-    axs[0].legend([f'Current: {round(current[-1], 2)}A'], loc='upper left')
-    axs[1].legend([f'Voltage: {round(voltage[-1], 2)}V'], loc='upper left')
-    axs[2].legend([f'Power: {round(power[-1], 2)} W'], loc="upper left")
+    axs[0].legend([f"Current: {round(current[-1], 2)}A"], loc="upper left")
+    axs[1].legend([f"Voltage: {round(voltage[-1], 2)}V"], loc="upper left")
+    axs[2].legend([f"Power: {round(power[-1], 2)} W"], loc="upper left")
 
     # Set x-axis limits to show only the last 10 seconds of data
     if len(x_val) > 10:
@@ -59,5 +44,27 @@ def updateValues(i):
 
     plt.tight_layout()
 
-ani = FuncAnimation(fig, updateValues, interval=intervalValue)
-plt.show()
+
+def main(interval_val, graph_title):
+    # Arrays for sensor data
+    current = []
+    voltage = []
+    power = []
+    x_val = []
+
+    index = count()  # counter
+
+    fig, axs = plt.subplots(3)
+    fig.suptitle(graph_title)
+
+    ani = FuncAnimation(
+        fig,
+        updateValues,
+        fargs=[axs, x_val, index, current, voltage, power],
+        interval=interval_val,
+    )
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(500, "Drone Control")
